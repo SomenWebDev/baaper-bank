a62b5394aaec3ce4d3bff46aee058432f9a2be27
Somen Singha <somensingha06@gmail.com>
2022-02-12 16:18:54 +0600

    added

---
+++ js/banking.js
@@ -0,0 +1,10 @@
+document
+  .getElementById("deposit-button")
+  .addEventListener("click", function () {
+    const depositInput = document.getElementById("deposit-input");
+    const depositAmount = depositInput.value;
+    console.log(depositAmount);
+    const depositTotal = document.getElementById("deposit-total");
+    depositTotal.innerText = depositAmount;
+    depositInput.value = "";
+  });
