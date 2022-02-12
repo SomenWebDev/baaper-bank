94bb3e3f662b3cfae228afb465bb62be3c2a9b64
Somen Singha <somensingha06@gmail.com>
2022-02-13 01:12:25 +0600

    added

--- js/banking.js
+++ js/banking.js
@@ -2,9 +2,43 @@ document
   .getElementById("deposit-button")
   .addEventListener("click", function () {
     const depositInput = document.getElementById("deposit-input");
-    const depositAmount = depositInput.value;
-    console.log(depositAmount);
+    const newDepositAmount = depositInput.value;
+    console.log(newDepositAmount);
     const depositTotal = document.getElementById("deposit-total");
-    depositTotal.innerText = depositAmount;
+    console.log(depositTotal);
+    const previousDepositAmount = depositTotal.innerText;
+    const newDepositTotal =
+      parseFloat(previousDepositAmount) + parseFloat(newDepositAmount);
+
+    depositTotal.innerText = newDepositTotal;
+
+    // update account balance
+    const balanceTotal = document.getElementById("balance-total");
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previousBalanceTotal + parseFloat(newDepositAmount);
+    balanceTotal.innerText = newBalanceTotal;
+
     depositInput.value = "";
+  });
+
+document
+  .getElementById("withdraw-button")
+  .addEventListener("click", function () {
+    const withdrawInput = document.getElementById("withdraw-input");
+    const withdrawAmountText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(withdrawAmountText);
+    console.log(newWithdrawAmount);
+
+    const withdrawTotal = document.getElementById("withdraw-total");
+    const previousWithdrawText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawText);
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotal.innerText = newWithdrawTotal;
+    const balanceTotal = document.getElementById("balance-total");
+    const previousBalanceText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceText);
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotal.innerText = newBalanceTotal;
+    withdrawInput.value = "";
   });
